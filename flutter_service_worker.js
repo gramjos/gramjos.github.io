cdcda1008e946870737fd2a44a24c678c2cab497
gramjos <graham.joss@gmail.com>
2024-01-16 13:25:22 -0600

    get

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "91ba842f0cc32f36db7907b113e3d0a8",
-"index.html": "48fc112e9a4527dab04e796e53c04be5",
-"/": "48fc112e9a4527dab04e796e53c04be5",
-"main.dart.js": "bbe67c9e9515fde5761bfffcfb6d4d54",
+"index.html": "3682ce3d63b78dce68347ccdd6796ac6",
+"/": "3682ce3d63b78dce68347ccdd6796ac6",
+"main.dart.js": "87bca5430834fff791ed2cfd325ceff5",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
