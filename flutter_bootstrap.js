750629b08b41c13dd9939b4d805b0369b21f1a6a
gramjos <graham.joss@gmail.com>
2024-06-16 17:09:22 -0500

    image rotating

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4170515660"
+    serviceWorkerVersion: "1626110408"
   }
 });
