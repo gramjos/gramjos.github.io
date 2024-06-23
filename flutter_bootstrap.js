012393cf589e44256230112146b12eafa5d8b572
gramjos <graham.joss@gmail.com>
2024-06-23 13:40:17 -0500

    libary version controlling

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1211347282"
+    serviceWorkerVersion: "2887063611"
   }
 });
