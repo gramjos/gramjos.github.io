289e372cae1cd535ef428b99e318cec595a08135
gramjos <graham.joss@gmail.com>
2024-10-02 12:39:04 -0500

    starting 2.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3416788562"
+    serviceWorkerVersion: "2521523650"
   }
 });
